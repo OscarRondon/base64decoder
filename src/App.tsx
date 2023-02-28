5a0e752b71fec2e9f346beddd23919e371b425f2
orondon <oscar.rondon.c@gmail.com>
2023-02-28 11:44:18 -0300

    - Styling main - Add options for Encode/Decode

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,23 @@ import './App.css'
 
 function App () {
   return (
-    <div className='App'>
-      <main>
-        <h1>Base 64 Decoder</h1>
-      </main>
-    </div>
+
+    <main className='main'>
+      <h1>Base 64 Decoder</h1>
+      <section className='options'>
+        <div className='options'>
+          <label>
+            <input type='radio' id='rd_encode' name='action' placeholder='Encode' value='encode' checked/>
+            <span>Encode</span>
+          </label>
+          <label>
+            <input type='radio' id='rd_decode' name='action' placeholder='Decode' value='decode'/>
+            <span>Encode</span>
+          </label>
+        </div>
+      </section>
+      <div />
+    </main>
   )
 }
 
