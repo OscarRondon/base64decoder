e1f7c1c1ca7b4de9e94256c6e89c8ec779cf7ff1
orondon <oscar.rondon.c@gmail.com>
2023-03-07 11:16:22 -0300

    radio buttons styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,35 @@
 import './App.css'
 
-function App () {
+const App = (): JSX.Element => {
   return (
 
     <main className='main'>
-      <h1>Base 64 Decoder</h1>
+      <h1 className='title'>Base 64 Decoder</h1>
       <section className='options'>
-        <div className='options'>
-          <label>
+          <label className='option-check'>
             <input type='radio' id='rd_encode' name='action' placeholder='Encode' value='encode' checked/>
             <span>Encode</span>
           </label>
-          <label>
+          <label className='option-check'>
             <input type='radio' id='rd_decode' name='action' placeholder='Decode' value='decode'/>
-            <span>Encode</span>
+            <span>Decode</span>
           </label>
+      </section>
+      <section className='inputs'>
+        <div className='input-container'>
+          <header>
+            <label>Input:</label>
+          </header>
+          <input type='text' id='txt_inputData' placeholder='Write a text' />
+        </div>
+        <div>
+          <button>Encode</button>
+        </div>
+        <div className='input-container'>
+          <header>
+            <label>Output:</label>
+          </header>
+          <input type='text' id='txt_outputData' placeholder='Result text' />
         </div>
       </section>
       <div />
