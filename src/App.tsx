ab92d0a57481f76dc00e944f5087ce2bed5ffd85
orondon <oscar.rondon.c@gmail.com>
2023-03-17 21:28:37 -0300

    RadioButtons actions

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
+import { useState } from 'react'
 import './App.css'
 
-const App = (): JSX.Element => {
-  return (
+type Action = 'Encode' | 'Decode'
 
+const App = (): JSX.Element => {
+  const [typeOfAction, setTypeOfAction] = useState<Action>('Encode')
+
+  const handleRadioButtonOnChange = (action: Action): void => {
+    setTypeOfAction(action)
+  }
+
+  return (
     <main className='main'>
       <h1 className='title'>Base 64 Decoder</h1>
       <section className='options'>
           <label className='option-check'>
-            <input type='radio' id='rd_encode' name='action' placeholder='Encode' value='encode'/>
+            <input type='radio' id='rd_encode' name='action' placeholder='Encode' value='encode'
+            checked = {typeOfAction === 'Encode'}
+            onChange={() => { handleRadioButtonOnChange('Encode') }} />
             <span>Encode</span>
           </label>
           <label className='option-check'>
-            <input type='radio' id='rd_decode' name='action' placeholder='Decode' value='decode'/>
+            <input type='radio' id='rd_decode' name='action' placeholder='Decode' value='decode'
+            checked = {typeOfAction !== 'Encode'}
+            onChange={() => { handleRadioButtonOnChange('Decode') }} />
             <span>Decode</span>
           </label>
       </section>
@@ -20,9 +32,9 @@ const App = (): JSX.Element => {
           <header>
             <label>Input</label>
           </header>
-          <textarea id='txt_inputData' placeholder='Write a text' />
+          <textarea id='txt_inputData' placeholder='Write a text'/>
         </div>
-        <button type='button'>Encode</button>
+        <button type='button'>{typeOfAction}</button>
         <div className='input-container'>
           <header>
             <label>Output</label>
@@ -31,7 +43,7 @@ const App = (): JSX.Element => {
               <img title='Check' src='../src/assets/icons/double-tick-icon.svg' className='display-none'/>
             </button>
           </header>
-          <textarea id='txt_outputData' placeholder='Result text' />
+          <textarea id='txt_outputData' placeholder='Result text' readOnly />
         </div>
       </section>
       <div />
