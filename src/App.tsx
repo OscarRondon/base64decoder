7f2688c6d3b65fcebc63cc799c32ad1fcb835305
orondon <oscar.rondon.c@gmail.com>
2023-03-25 12:18:40 -0300

    CopyToClipboard functionality

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,26 @@ type Action = 'Encode' | 'Decode'
 
 const App = (): JSX.Element => {
   const [typeOfAction, setTypeOfAction] = useState<Action>('Encode')
+  const [textData, setTextData] = useState('')
 
   const handleRadioButtonOnChange = (action: Action): void => {
     setTypeOfAction(action)
+  }
+
+  const handleBtnEncodeDecodeClick = (): void => {
+    const newTextData = (document.getElementById('txtInputData') as HTMLTextAreaElement).value
+    if (newTextData !== '') {
+      setTextData(newTextData)
+    }
+  }
+
+  const handleBtnCopyToClipboardClick = (): void => {
+    const textData = (document.getElementById('txtInputData') as HTMLTextAreaElement).value
+    if (textData !== '') {
+      navigator.clipboard.writeText(textData)
+        .then(res => { console.log('copiado') })
+        .catch(err => { console.log(err) })
+    }
   }
 
   return (
@@ -15,13 +32,13 @@ const App = (): JSX.Element => {
       <h1 className='title'>Base 64 Decoder</h1>
       <section className='options'>
           <label className='option-check'>
-            <input type='radio' id='rd_encode' name='action' placeholder='Encode' value='encode'
+            <input type='radio' id='rdEncode' name='action' placeholder='Encode' value='encode'
             checked = {typeOfAction === 'Encode'}
             onChange={() => { handleRadioButtonOnChange('Encode') }} />
             <span>Encode</span>
           </label>
           <label className='option-check'>
-            <input type='radio' id='rd_decode' name='action' placeholder='Decode' value='decode'
+            <input type='radio' id='rdDecode' name='action' placeholder='Decode' value='decode'
             checked = {typeOfAction !== 'Encode'}
             onChange={() => { handleRadioButtonOnChange('Decode') }} />
             <span>Decode</span>
@@ -32,18 +49,18 @@ const App = (): JSX.Element => {
           <header>
             <label>Input</label>
           </header>
-          <textarea id='txt_inputData' placeholder='Write a text'/>
+          <textarea id='txtInputData' placeholder='Write a text'/>
         </div>
-        <button type='button'>{typeOfAction}</button>
+        <button type='button' id='btnEcodeDecode' onClick={() => { handleBtnEncodeDecodeClick() }}>{typeOfAction}</button>
         <div className='input-container'>
           <header>
             <label>Output</label>
-            <button id='copyToClipbooard' title='Copy to Clipboard' className='button-copy display-none'>
-              <img title='Copy' src='../src/assets/icons/copy-to-clipboard.svg' className='display-none'/>
-              <img title='Check' src='../src/assets/icons/double-tick-icon.svg' className='display-none'/>
+            <button id='copyToClipbooard' title='Copy to Clipboard' className='button-copy' onClick={() => { handleBtnCopyToClipboardClick() }}>
+              <img id='copy' title='Copy' src='../src/assets/icons/copy-to-clipboard.svg'/>
+              <img id='check' title='Check' src='../src/assets/icons/double-tick-icon.svg' className='display-none'/>
             </button>
           </header>
-          <textarea id='txt_outputData' placeholder='Result text' readOnly />
+          <textarea id='txtOutputData' placeholder='Result text' value={textData} readOnly />
         </div>
       </section>
       <div />
