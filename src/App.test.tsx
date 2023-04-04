89b317b25db9711ece439a7c55a15581574f9c45
orondon <oscar.rondon.c@gmail.com>
2023-04-04 19:47:40 -0400

    Improves on Encode and Decode tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,16 +1,20 @@
 import { test, expect } from 'vitest'
-import { render } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import App from './App'
+// import { render } from '@testing-library/react'
+// import userEvent from '@testing-library/user-event'
+// import App from './App'
 import { Utf8toB64, B64ToUtf8 } from './logic/encodingB64'
 
-test('Dom Test', async () => {
-  const user = userEvent.setup()
-  const app = render(<App />)
+test('Dom  Test', async () => {
+  // const user = userEvent.setup()
+  // const app = render(<App />)
+})
 
+test('Encode  Test', async () => {
   const encodeData = Utf8toB64('Clave 1 test unitario')
   expect(encodeData).toEqual('Q2xhdmUgMSB0ZXN0IHVuaXRhcmlv')
+})
 
+test('Decode  Test', async () => {
   const decodeData = B64ToUtf8('Q2xhdmUgMSB0ZXN0IHVuaXRhcmlv')
   expect(decodeData).toEqual('Clave 1 test unitario')
 })
