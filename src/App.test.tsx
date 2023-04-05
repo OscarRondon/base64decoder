684a3b32db8b35225c8b2e015e8d5fbab85002fd
orondon <oscar.rondon.c@gmail.com>
2023-04-05 19:17:48 -0400

    Testing [DOM testisng completed]

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,19 @@
 import { test, expect } from 'vitest'
-// import { render } from '@testing-library/react'
-// import userEvent from '@testing-library/user-event'
-// import App from './App'
+import { render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import App from './App'
 import { Utf8toB64, B64ToUtf8 } from './logic/encodingB64'
 
 test('Dom  Test', async () => {
-  // const user = userEvent.setup()
-  // const app = render(<App />)
+  const user = userEvent.setup()
+  const app = render(<App />)
+
+  const txtInputData = app.container.querySelector('#txtInputData')
+  const btnEcodeDecode = app.container.querySelector('#btnEcodeDecode')
+  if (txtInputData !== null) { await user.type(txtInputData, 'Clave 1 test unitario') }
+  if (btnEcodeDecode != null) { await userEvent.click(btnEcodeDecode) }
+  const result = await app.findByDisplayValue(/Q2xhdmUgMSB0ZXN0IHVuaXRhcmlv/i, {}, { timeout: 5000 })
+  expect(result).toBeTruthy()
 })
 
 test('Encode  Test', async () => {
